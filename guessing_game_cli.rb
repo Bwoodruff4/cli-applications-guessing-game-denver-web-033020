cb17c037467ce3096e1bacb46ed629e237516a42
Blake <blakewoodruff4@gmail.com>
2020-03-23 22:19:44 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,5 +1,12 @@
 def run_guessing_game
 	rand_num=rand(1..6).to_s
 	input=gets.chomp
+	
+	if input == 'exit'
+	  puts "Goodbye!"
+	elsif input == rand_num
+	    puts "You guessed the correct number!"
+	  elsif input != rand_num
+	  
 end 
 
