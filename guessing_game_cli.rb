c102d2ad8e6630536ea793efcf81af19e0a54b7b
Blake <blakewoodruff4@gmail.com>
2020-03-23 22:14:34 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,6 +1,6 @@
 def run_guessing_game
-	puts "/Guess a number betwen 1 and 6."
-	roll = rand(6)
+	puts "Guess a number betwen 1 and 6.\n"
+	roll = rand(1..6)
 	user_input = gets.chomp.to_s
     if user_input == roll 
     	return "You guessed the correct number!"
@@ -8,10 +8,6 @@ def run_guessing_game
     	return "Sorry! The computer guessed #{roll}."
     elsif user_input == "exit"
       puts "Goodbye!"
-    else
-      puts "Invalid input "
     end 
-    
-  
 end 
 
